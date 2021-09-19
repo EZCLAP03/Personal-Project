e639abf55939dbfcbb9f5d5db685c9f3c2bcd886
Harisai <64903659+EZCLAP03@users.noreply.github.com>
2021-09-19 10:37:43 +0530

    main

---
+++ Personal Project - Social Media Website/client.py
@@ -0,0 +1,33 @@
+import socket
+import tkinter 
+import threading
+from tkinter import scrolledtext
+from tkinter import simpledialog
+
+HOST = " "
+PORT = 9090
+
+class Client: 
+    def __init__(self, host, port):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host, port))
+
+        msg = tkinter.Tk()
+        msg.withdraw()
+
+        self.nickname = simpledialog.askstring("Nickname", "please choose a nickname", parent = msg)
+
+        self.gui_done = False
+        self.running = True
+
+        gui_thread = threading.Thread(target=self.gui_loop)
+        receive_thread = threading.Thread(target=self.receive)
+
+        gui_thread.start()
+        receive_thread.start()
+
+    def gui_loop(self):
+        pass
+
+    def receive(self):
+        pass
